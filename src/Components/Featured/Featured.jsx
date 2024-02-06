f12d3adf8fc748f143b3e01290bb1cc9140c2c5b
Gobihanath Balasubramaniam <126072422+Gobihanath@users.noreply.github.com>
2024-02-06 08:57:16 +0530

    Third Commit by gobi

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -1,35 +1,24 @@
 import React from 'react'
-import './Featured.css'
 import Video2 from '../Assets/video (2160p) (1).mp4'
+import './Featured.css'
 
 const Featured = () => {
   return (
     <div className='feature-container'>
-      <div className='content'>
-        <h1>Who We Are</h1>
+      <div className='head'>
+        <h1>Who we are</h1>
         <hr/>
-      
-        <section className="quote">
-            <div className="row1">
-              <div className="quote-image">
-                  <video src={Video2} autoPlay loop muted></video>
-              </div>
-            <div className="quote-abt">
-            <h1>Life on Two Wheels!</h1>
-            <h1>Life on Two Wheels!</h1>
-            <div className="line"></div>
-                    <p>“On a motorcycle, you can’t really think about more than where you are. There’s a freedom that comes with that – from stress, worry, sweating the small stuff.” ― <b>Laurence Fishburne</b></p>
-                    <p> “Animals travel on all fours. Mankind on two. Motorcycling is not a means of transport but an ideology, a nomadic way of life.” ― <b>Amit Reddy</b></p>
-                    <p>“Love is the feeling you get when you like something as much as your motorcycle.” ― <b>Hunter S. Thompson</b></p>
-                    <p>“Life is not about waiting for the storms to pass: it’s about learning how to ride in the rain!” - <b>Unknown</b></p>
-            </div>
-          
-            </div> 
-       </section>
-
+        <div className='row'>
+          <div className='left-r'>
+            <video src={Video2} alt='' autoPlay muted/>
+          </div>
+          <div className='right-r'>
+           <h2>Crafting Legacies, Inspiring Journeys: "BRANDBLITZ Your Partner in Triumph"</h2>
+           <p>We are a top independent media planning, buying, and advertising company that supports you in combining your creativity with smart media placement. We are more than just a marketing agency; "we are architects of brand experiences, storytellers of compelling narratives, and strategic partners in your journey to success".</p>
+           <p>We believe in the power of authenticity, and our mission is to amplify the essence of your brand, making it resonate with your audience on a profound level. With a focus on results-driven strategies, creative brilliance, and a genuine passion for what we do, we are here to elevate your brand to new heights. Welcome to a partnership where your success is our purpose.</p>
+          </div>
+          </div>
       </div>
-
-
     </div>
   )
 }
