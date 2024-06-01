bbebcb45c2f10ea4946caf4672fdf1a8c9fa3301
dilansakila <dilansakila99@gmail.com>
2024-06-01 14:41:14 +0530

    second commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
        <li onClick={()=>{setMenu("home")}}><Link style={{textDecoration:'none'}} to='/'>Home</Link> {menu==="home"?<hr/>:<></>}</li>
        <li onClick={()=>{setMenu("services")}}><Link style={{textDecoration:'none'}} to='/Ourservices'>Our Services</Link>{menu==="services"?<hr/>:<></>}</li>
        <li onClick={()=>{setMenu("client")}}><Link style={{textDecoration:'none'}} to=''>Clients</Link>{menu==="client"?<hr/>:<></>}</li>
-       <li onClick={()=>{setMenu("about")}}><Link style={{textDecoration:'none'}} to=''>About Us</Link>{menu==="about"?<hr/>:<></>}</li>
+       <li onClick={()=>{setMenu("about")}}><Link style={{textDecoration:'none'}} to='AboutUs'>About Us</Link>{menu==="about"?<hr/>:<></>}</li>
        <li onClick={()=>{setMenu("login")}}><Link  style={{textDecoration:'none'}} to='/login'>Login </Link>{menu==="login"?<hr/>:<></>}  </li>
       </ul>
 
